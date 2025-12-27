21c5d3e3de7da33135bfc68c0f577784ff929a34
Mahdiiix <mahdi.kazemi@anlyx.de>
2025-12-27 15:03:12 +0100

    Define END node and get all nodes

--- src/node.py
+++ src/node.py
@@ -1,11 +1,55 @@
-from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, Union, Callable, Self
+import dspy
 
+class Node:
+    _registry: dict[str, Self] = {}
 
-class Node(ABC):
-    def __init__(self, name: Optional[str] = None):
-        self.name = name if name else self.__class__.__name__
+    @classmethod
+    def get_node(cls, name: str) -> Self:
+        try:
+            return cls._registry[name]
+        except KeyError:
+            raise ValueError(f"Node {name} not found")
 
-    @abstractmethod
-    def run(self):
-        pass
+    @classmethod
+    def _register_node(cls, name: str, node: Self):
+        if name in cls._registry:
+            raise ValueError(f"Node {name} already exists")
+        cls._registry[name] = node
+
+    @classmethod
+    def get_nodes(cls):
+        yield from cls._registry.items()
+
+    def __init__(self, name: str, fn: Callable[..., str], program: Optional[dspy.Module] = None, freeze: bool = False):
+        self.name = name
+        self.llm_program = program
+        self.run = fn
+        self.freeze = freeze
+        self._register_node(name, self)
+
+    def __call__(self, *args, **kwargs):
+        return self.run(*args, **kwargs, program = self.llm_program)
+
+def create_node(name: str, program: Optional[dspy.Module] = None):
+    def decorator(fn: Callable[..., str]):
+        return Node(name, fn, program)
+    return decorator
+
+END = Node("END", lambda _: "END", freeze=True)
+
+if __name__ == "__main__":
+    llm = dspy.Module()
+    @create_node("test", llm)
+    def f(something: int, program: dspy.Module) -> int:
+       print(type(program))
+       return something
+    print(f(5))
+    print(f.name)
+
+    @create_node("asynctest", llm)
+    async def af(something: int, program: dspy.Module) -> int:
+        print(type(program))
+        return something
+    import asyncio
+    print(asyncio.run(af(5)))
