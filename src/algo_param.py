f3d4826a6324856cf6c4c30dc0cbe2ddd6a58002
Mahdiiix <mahdi.kazemi@anlyx.de>
2025-12-27 15:00:58 +0100

    Add fetching and seting the value and next value

--- src/algo_param.py
+++ src/algo_param.py
@@ -8,7 +8,7 @@ class AlgoParam[T]:
 
     def __init__(self, mutate: MutationFunc[T], value: T, name: Optional[str] = None, freeze: bool = False):
         self.mutate = mutate
-        self.value = value
+        self._value = value
         self.name = name
         self.freeze = freeze
         AlgoParam._registry.add(self)
@@ -16,6 +16,20 @@ class AlgoParam[T]:
     @classmethod
     def parameters(cls):
         yield from cls._registry
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value: T):
+        if not self.freeze:
+            self._value = value
+
+    @property
+    def next_value(self):
+        self.value = self.mutate(self.value)
+        return self.value
 
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.name}, value={self.value}, mutate_func={getattr(self.mutate, '__name__', None)})"
@@ -25,7 +39,7 @@ if __name__ == "__main__":
     a = AlgoParam[int](ff, name="a", value=1)
     c = AlgoParam[int](lambda x: x * 2, value=2, name="c")
     def f():
-        return AlgoParam[str](lambda x: x * 2, value="hello", name="b")
+         AlgoParam[str](lambda x: x * 2, value="hello", name="b")
     f()
 
     for item in AlgoParam.parameters():
